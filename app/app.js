b7aeee463a68c4a764dc4478f59c64f7448ba18c
roche_d <roche_d@epitech.eu>
2015-05-18 15:28:52 +0200

    socket

--- app/app.js
+++ app/app.js
@@ -1,3 +1,27 @@
 /**
  * Created by roche_d on 17/05/15.
  */
+
+var prod = true;
+
+(function (angular, io) {
+    'use strict';
+
+
+    angular.module('garagoModule', []).
+        controller('RequestListCtrl', ['$scope', 'garagoRequestService', function ($scope, garagoRequestService, timeout) {
+            $scope.requests = [];
+            $scope.refreshRequests = function () {
+                $scope.requests = garagoRequestService.getRequests();
+            };
+
+            $scope.refreshRequests();
+
+            var url = (prod) ? ("http://garago.cleverapps.io/") : ("http://localhost:8080/");
+            /*var socket = io.connect(url);
+            socket.on('newRequest', function (data) {
+               console.log('new req from server');
+            });*/
+        }]);
+
+} (angular, io));
---
+++ app/garagoRequestService.js
@@ -0,0 +1,59 @@
+/**
+ * Created by roche_d on 18/05/15.
+ */
+
+(function (angular) {
+    'use strict';
+
+    function fmtDate(from, to) {
+        console.log('fmt ' + from + ' ' + to);
+        //return "plop";
+        if (from.length > 0 && to.length > 0) {
+            return "Between " + from + " and " + to;
+        } else if (from.length > 0) {
+            return "From " + from;
+        } else if (to.length > 0) {
+            return "Before " + to;
+        } else {
+            return "Any";
+        }
+    }
+
+    angular.module('garagoModule').
+        factory('garagoRequestService', ['$http', function (http) {
+            var url = (prod) ? ("http://garago.cleverapps.io") : ("http://127.0.0.1:8080");
+            return {
+                getRequests: function () {
+                    var result = [];
+                    http({
+                        method: 'GET',
+                        url: url + '/api/getAllRequest',
+                        params: {key: 'fslkj45k54kjh'}
+                    }).success(function (data) {
+                        console.log('success');
+                        console.log(data);
+                        if (data.res && data.data.requests) {
+                            data.data.requests.forEach(function (e) {
+                                var when = fmtDate(e.req.when['date-from'], e.req.when['date-to']);
+                                result.push({
+                                    name: 'Stephane',
+                                    insurance: 'Direct Assurance',
+                                    contract: 'Premium',
+                                    what: e.req.what[0],
+                                    when: when,
+                                    ccar: (e.req.courtesyCar) ? ('Yes') : ('No'),
+                                    where: e.req.where,
+                                    id: e.id
+                                });
+                            });
+                        }
+                    }).error(function (err) {
+                        console.log('error');
+
+                    });
+                    return result;
+                }
+            };
+        }]);
+
+} (angular));
