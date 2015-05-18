f3ef71014da7ad91f753d9db0efe31c7d6923608
roche_d <roche_d@epitech.eu>
2015-05-18 23:55:01 +0200

    garage answer

--- app/app.js
+++ app/app.js
@@ -11,17 +11,57 @@ var prod = true;
     angular.module('garagoModule', []).
         controller('RequestListCtrl', ['$scope', 'garagoRequestService', function ($scope, garagoRequestService, timeout) {
             $scope.requests = [];
+            $scope.msg = {};
             $scope.refreshRequests = function () {
                 $scope.requests = garagoRequestService.getRequests();
+            };
+            $scope.answerRequest = function (id) {
+                console.log('answer');
+                console.log(id);
+                $scope.selectedId = id;
+                $('#answerModal').openModal();
+                return ;
+                if (!id) return;
+
+            };
+            $scope.sendAnswerRequest = function (selected) {
+              console.log('ca rentre');
+                console.log(selected);
+                console.log($scope.msg);
+                if (!selected) return;
+                garagoRequestService.answerRequest({
+                    offerPrice: $scope.msg.price,
+                    offerText: $scope.msg.text,
+                    offerDate: $scope.msg.date,
+                    id: selected.id,
+                    garageName: 'Georges'
+                }, function (data) {
+                    console.log('success');
+                    console.log(data);
+                    $scope.refreshRequests();
+                    $scope.msg = {};
+                    $('#answerModal').closeModal();
+                }, function (err) {
+                    Materialize.toast('An error occurred, please check the informations !', 2000)
+                    console.log('something failed !');
+                    console.log(err);
+                });
+
             };
 
             $scope.refreshRequests();
 
-            var url = (prod) ? ("http://garago.cleverapps.io/") : ("http://localhost:8080/");
-            /*var socket = io.connect(url);
-            socket.on('newRequest', function (data) {
+            var url = (prod) ? ("http://garago.cleverapps.io/") : ("ws://127.0.0.1:8080/");
+            //var socket = io.connect(url);//{transports: ['websocket', 'polling', 'flashsocket']}
+            /*socket.on('newRequest', function (data) {
                console.log('new req from server');
             });*/
         }]);
+
+    $('.datepicker').pickadate({
+        selectMonths: true, // Creates a dropdown to control month
+        selectYears: 1, // Creates a dropdown of 15 years to control year
+        closeOnSelect: true
+    });
 
 } (angular, io));
--- app/garagoRequestService.js
+++ app/garagoRequestService.js
@@ -6,8 +6,6 @@
     'use strict';
 
     function fmtDate(from, to) {
-        console.log('fmt ' + from + ' ' + to);
-        //return "plop";
         if (from.length > 0 && to.length > 0) {
             return "Between " + from + " and " + to;
         } else if (from.length > 0) {
@@ -52,6 +50,31 @@
 
                     });
                     return result;
+                },
+                answerRequest: function (msg, success, fail) {
+                    console.log('post');
+                    console.log(msg);
+                    http({
+                        method: 'POST',
+                        url: url + '/api/answerRequest',
+                        data: {
+                            key: 'fslkj45k54kjh',
+                            id: msg.id,
+                            data: {
+                                price: msg.offerPrice,
+                                msg: msg.offerText,
+                                date: msg.offerDate
+                            }
+                        }
+                    }).success(function (data) {
+                        if (data.res) {
+                            success(data.data);
+                        } else {
+                            fail(data.data);
+                        }
+                    }).error(function (err) {
+                        console.log('post failed');
+                    });
                 }
             };
         }]);
